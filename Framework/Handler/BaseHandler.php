1bebb5e7b0da9d1da3d1ebd427dc0979b160db04
Anderson <lhongda@live.com>
2018-01-25 15:30:44 +0800

    support CORS for ant design backend.

--- Framework/Handler/BaseHandler.php
+++ Framework/Handler/BaseHandler.php
@@ -29,6 +29,11 @@ class BaseHandler
             'data' => $data
         ];
         $response = Di::getDefault()->get('response');
+        $response
+                ->setHeader('Access-Control-Allow-Origin', 'http://devel.isophp.cn:8000')
+                ->setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+                ->setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Range, Content-Disposition, Content-Type, Authorization')
+                ->setHeader('Access-Control-Allow-Credentials', 'true');
         $response->setContentType('application/json');
         $response->setContent(json_encode($ret));
         $response->send();
