7b8519b66ec2b34dc04c9f14e25e616a0c19ed6a
Anderson <lhongda@live.com>
2018-01-25 15:54:38 +0800

    solve request problem
    Add header and specify url in request.

--- ManagerPlatform/src/services/api.js
+++ ManagerPlatform/src/services/api.js
@@ -1,8 +1,11 @@
 import request from '../utils/request';
 
 export async function adminApiGate(params) {
-    return request(`/adminApiGate`, {
+    return request(`http://devel.isophp.cn/adminApiGate`, {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
         body: {
             ...params,
         }
